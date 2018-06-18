4040328bfc2fd17699c5ca8078ac0aaf267b0f08
Christopher Bradshaw <christopherbaxterbradshaw@gmail.com>
2018-06-18 15:53:10 -0500

    getManifest and getConfig working for client

--- client/client.go
+++ client/client.go
@@ -1,5 +1,8 @@
 package main
 
+import Models "../models"
+import "errors"
+import "encoding/json"
 import "bufio"
 import "fmt"
 import "os"
@@ -16,7 +19,7 @@ func init() {
 }
 
 func main() {
-  fmt.Println("--------- Virtual Filesystem ---------")
+  fmt.Println("\n--------- Virtual Filesystem ---------")
   reader := bufio.NewReader(os.Stdin)
   getServerInfo(reader)
   testConnection()
@@ -58,14 +61,14 @@ func testConnection() {
 
 func doPromptLoop(reader *bufio.Reader) {
   for ;; {
-    fmt.Println("----- What would you like to do? -----")
+    fmt.Println("\n----- What would you like to do? -----")
     fmt.Println("1. View server manifest")
     fmt.Println("2. Upload a file")
     fmt.Println("3. Download a file")
     fmt.Println("4. Remove a file")
-    fmt.Println("5. View server configuration")
+    fmt.Println("5. View server settings")
     fmt.Println("6. Exit")
-    fmt.Print("Selection (1-6): ")
+    fmt.Print("\nSelection (1-6): ")
 
     selectionStr, _ := reader.ReadString('\n')
     selectionInt , err := strconv.Atoi(strings.TrimSpace(selectionStr))
@@ -101,8 +104,41 @@ func doPromptLoop(reader *bufio.Reader) {
   }
 }
 
-func printManifest() {
+func getManifest() (*Models.GetManifestResponse, error) {
+  resp, err := http.Get(url + "/getManifest")
+  if err != nil {
+    fmt.Printf("%s", err)
+    return nil, errors.New("Could not retrieve manifest")
+  }
+  defer resp.Body.Close()
 
+  // Decode response data
+  var decodedResponse Models.GetManifestResponse
+  decoder := json.NewDecoder(resp.Body)
+  err = decoder.Decode(&decodedResponse)
+  if err != nil {
+    fmt.Printf("%s",err)
+    return nil, errors.New("Could not decode manifest response")
+  }
+
+  return &decodedResponse, nil
+}
+
+// Request the manifest and print it
+func printManifest() {
+  fmt.Println("\n----- File listing -----")
+  manifest, err := getManifest()
+
+  // Could not get manifest
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+
+  // Got the manifest, display them nicely
+  for _, elm := range manifest.FileEntries {
+    fmt.Println(elm)
+  }
 }
 
 func uploadFile() {
@@ -117,6 +153,36 @@ func removeFile() {
 
 }
 
-func printConfig() {
+func getConfig() (*Models.GetConfigResponse, error) {
+  resp, err := http.Get(url + "/getConfig")
+  if err != nil {
+    fmt.Printf("%s", err)
+    return nil, errors.New("Could not retrieve config")
+  }
+  defer resp.Body.Close()
 
+  // Decode response data
+  var decodedResponse Models.GetConfigResponse
+  decoder := json.NewDecoder(resp.Body)
+  err = decoder.Decode(&decodedResponse)
+  if err != nil {
+    fmt.Printf("%s",err)
+    return nil, errors.New("Could not decode manifest response")
+  }
+
+  return &decodedResponse, nil
+}
+
+func printConfig() {
+  fmt.Println("\n----- Server settings -----")
+  config, err := getConfig()
+
+  // Could not get manifest
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+
+  // Got the config, display it nicely
+  fmt.Println(config.ServerConfig)
 }
